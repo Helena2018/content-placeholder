0d72a9fd332237b441f1bdedd27d1eec51b9261e
Helena2018 <helena2020liu@gmail.com>
2022-07-11 15:02:54 +1200

    animation holding profile

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const header = document.getElementById('header')
 const title = document.getElementById('title')
 const excerpt = document.getElementById('excerpt')
-const profile_img = document.getElementById('profile_img')
+const profile_img = document.getElementById('profile-img')
 const name = document.getElementById('name')
 const date = document.getElementById('date')
 
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 1000)
 
 function getData() {
   header.innerHTML = `<img src="https://photos5.appleinsider.com/gallery/46342-93914-Pink-MacBook-Air-xl.jpg" alt=""/>`
@@ -15,4 +17,7 @@ function getData() {
   profile_img.innerHTML = `<img src="https://randomuser.me/api/portraits/women/26.jpg" alt="">`
   name.innerHTML = `John Doe`
   date.innerHTML = `Jul 11, 2022`
+
+  animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach(text => text.classList.remove('animated-bg-text'))
 }
