b9036c06babd246fc9a339793a1d97a643216edf
Helena2018 <helena2020liu@gmail.com>
2022-07-11 14:47:36 +1200

    updated js

---
+++ script.js
@@ -0,0 +1,18 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bgs_text = document.querySelectorAll('.animated-bg-text')
+
+function getData() {
+  header.innerHTML = `<img src="https://photos5.appleinsider.com/gallery/46342-93914-Pink-MacBook-Air-xl.jpg" alt=""/>`
+  title.innerHTML = `Lorem ipsum dolor sit amet`
+  excerpt.innerHTML = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus, aliquam sit tempora odio ex magni.`
+  profile_img.innerHTML = `<img src="https://randomuser.me/api/portraits/women/26.jpg" alt="">`
+  name.innerHTML = `John Doe`
+  date.innerHTML = `Jul 11, 2022`
+}
